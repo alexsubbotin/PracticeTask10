0b5052128b133dd2be860034163286350c04bb20
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 15:44:34 +0500

    Method to create a random tree developed

--- PracticeTask10/PracticeTask10/Tree.cs
+++ PracticeTask10/PracticeTask10/Tree.cs
@@ -17,5 +17,44 @@ namespace PracticeTask10
         {
             this.Root = Root;
         }
+
+
+        // Creating random tree.
+        public Random rnd = new Random();
+        public void CreateRandomTree()
+        {
+            int size = rnd.Next(1, 10);
+
+            this.Root = RandomTree(size);
+        }
+        private PointTree RandomTree(int size)
+        {
+            // Temporary object.
+            PointTree root;
+
+            // The number of elements in the left and in the right branches.
+            int nl, nr;
+
+            // If size=0 then it's the end of the tree.
+            if (size == 0)
+            {
+                root = null;
+                return root;
+            }
+
+            // Getting the sizes of branches.
+            nl = size / 2;
+            nr = size - nl - 1;
+
+            // Creating a random value for the root.
+            int d = rnd.Next(20, 51);
+            root = new PointTree(d);
+
+            // Building the branches.
+            root.Left = RandomTree(nl);
+            root.Right = RandomTree(nr);
+
+            return root;
+        }
     }
 }
