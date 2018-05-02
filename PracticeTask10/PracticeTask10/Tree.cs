9df72975d608051bad3a4a97110ddae555526648
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 15:29:32 +0500

    Class Tree added

---
+++ PracticeTask10/PracticeTask10/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask10
+{
+    // Class for the tree.
+    class Tree
+    {
+        // Root.
+        PointTree Root { get; set; }
+
+        // Constructor with a parameter.
+        public Tree(PointTree Root)
+        {
+            this.Root = Root;
+        }
+    }
+}
