393f6d35a05a33c0c1b6e1a3c09ab9d955037c0f
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 15:23:08 +0500

    Class PointTree added

---
+++ PracticeTask10/PracticeTask10/PointTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask10
+{
+    // Class for elements of a tree.
+    class PointTree
+    {
+        // Value.
+        public int Value { get; set; }
+
+        // Next element on the left.
+        public PointTree Left { get; set; }
+
+        // Next element of the right.
+        public PointTree Right { get; set; }
+    }
+}
