bc9e7de9a9be12ee34c6415b9508f6b0c20fa65b
Alexey Subbotin <subbotin910@gmail.com>
2018-05-02 15:45:17 +0500

    ..

--- PracticeTask10/PracticeTask10/PointTree.cs
+++ PracticeTask10/PracticeTask10/PointTree.cs
@@ -18,7 +18,7 @@ namespace PracticeTask10
         // Next element on the right.
         public PointTree Right { get; set; }
 
-        // Constructor with a parameter.
+        // Constructor with parameters.
         public PointTree(int value)
         {
             this.Value = value;
